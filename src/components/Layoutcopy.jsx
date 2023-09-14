cbeacf4da54cd071f12c945da6f78c68d7c50b42
TheOksigen <david@div.edu.az>
2023-09-14 20:22:09 +0400

    modified:   src/components/Layoutcopy.jsx

--- src/components/Layoutcopy.jsx
+++ src/components/Layoutcopy.jsx
@@ -263,6 +263,63 @@ const Layoutcopy = () => {
   const [collapsed, setCollapsed] = useState(false);
   const { token: { colorBgContainer }, } = theme.useToken();
   return (
+    //<Layout className='h-full'>
+    //  <Sider trigger={null} collapsible collapsed={collapsed}>
+    //    <div className="demo-logo-vertical" />
+    //    <Menu
+    //      theme="dark"
+    //      mode="inline"
+    //      defaultSelectedKeys={['1']}
+    //      items={[
+    //        {
+    //          key: '1',
+    //          icon: <UserOutlined />,
+    //          label: 'nav 1',
+    //        },
+    //        {
+    //          key: '2',
+    //          icon: <VideoCameraOutlined />,
+    //          label: 'nav 2',
+    //        },
+    //        {
+    //          key: '3',
+    //          icon: <UploadOutlined />,
+    //          label: 'nav 3',
+    //        },
+    //      ]}
+    //    />
+    //  </Sider>
+    //  <Layout>
+    //    <Header
+    //      style={{
+    //        padding: 0,
+    //        background: colorBgContainer,
+    //      }}
+    //    >
+    //      <Button
+    //        type="text"
+    //        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+    //        onClick={() => setCollapsed(!collapsed)}
+    //        style={{
+    //          fontSize: '16px',
+    //          width: 64,
+    //          height: 64,
+    //        }}
+    //      />
+    //    </Header>
+    //    <Content
+    //      style={{
+    //        margin: '24px 16px',
+    //        padding: 24,
+    //        minHeight: 280,
+    //        background: colorBgContainer,
+    //      }}
+    //    >
+    //      Content
+    //    </Content>
+    //  </Layout>
+    //</Layout>
+
     <Layout className='h-full'>
       <Sider trigger={null} collapsible collapsed={!collapsed}>
         <Button type="text" icon={collapsed ? <MenuUnfoldOutlined className='text-white' /> : <MenuFoldOutlined className='text-white' />} onClick={() => setCollapsed(!collapsed)} className='h-16 !w-full text-base m-auto' />
@@ -273,6 +330,7 @@ const Layoutcopy = () => {
                 key: index,
                 icon: item.icon,
                 label: item.name,
+                
               }
             })
           }
@@ -299,6 +357,8 @@ const Layoutcopy = () => {
         </Content>
       </Layout>
     </Layout>
-  );
+  )
+
+
 };
 export default Layoutcopy;
