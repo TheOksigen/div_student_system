06cb4112965f89cb24862135f5a94fe51c9f2a6e
TheOksigen <david@div.edu.az>
2023-09-15 02:28:25 +0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Calendar.jsx 	modified:   src/components/Layoutcopy.jsx 	modified:   src/components/Teams.jsx 	new file:   src/elements/User.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,7 +1,102 @@
-import React from 'react'
+import React from 'react';
+import { Badge, Calendar } from 'antd';
 
-export default function Calendar() {
-  return (
-	<div>Calendar</div>
-  )
-}
+
+const getListData = (value) => {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        {
+          type: 'warning',
+          content: 'Tutaqki burda front dersi var',
+        },
+        {
+          type: 'success',
+          content: 'Burdada nese',
+        },
+      ];
+      break;
+    case 10:
+      listData = [
+        {
+          type: 'warning',
+          content: 'Neseler...',
+        },
+        {
+          type: 'success',
+          content: 'Neseler...',
+        },
+        {
+          type: 'error',
+          content: 'Neseler...',
+        },
+      ];
+      break;
+    case 15:
+      listData = [
+        {
+          type: 'warning',
+          content: 'Neseler...',
+        },
+        {
+          type: 'success',
+          content: 'Neseler...',
+        },
+        {
+          type: 'error',
+          content: 'Neseler...',
+        },
+        {
+          type: 'error',
+          content: 'Neseler...',
+        },
+        {
+          type: 'error',
+          content: 'Neseler...',
+        },
+        {
+          type: 'error',
+          content: 'Neseler...',
+        },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+};
+const getMonthData = (value) => {
+  if (value.month() === 8) {
+    return 1394;
+  }
+};
+const Calendarr = () => {
+  const monthCellRender = (value) => {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Backlog number</span>
+      </div>
+    ) : null;
+  };
+  const dateCellRender = (value) => {
+    const listData = getListData(value);
+    return (
+      <ul className="events">
+        {listData.map((item) => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  const cellRender = (current, info) => {
+    if (info.type === 'date') return dateCellRender(current);
+    if (info.type === 'month') return monthCellRender(current);
+    return info.originNode;
+  };
+  return <Calendar cellRender={cellRender} />;
+};
+export default Calendarr;
--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
-
-function Teams() {
-  return (
-	<div>Teams</div>
-  )
-}
-
-export default Teams
+import React from 'react';
+import { Space, Table, Tag } from 'antd';
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  //{
+  //  title: 'Action',
+  //  key: 'action',
+  //  render: (_, record) => (
+  //    <Space size="middle">
+  //      <a>Invite {record.name}</a>
+  //      <a>Delete</a>
+  //    </Space>
+  //  ),
+  //},
+];
+const data = [
+  {
+    key: '1',
+    name: 'David Memmdov',
+    age: 19,
+    address: '',
+    tags: ['nice', 'Mentor'],
+  },
+  {
+    key: '2',
+    name: 'Vahid Qasim',
+    age: 42,
+    address: 'Nabran',
+    tags: ['nice', "Teacher"],
+  },
+  {
+    key: '3',
+    name: 'Mirefqan',
+    age: 24,
+    address: 'Razin',
+    tags: ['cool', 'mentor'],
+  },
+];
+const Team = () => <Table columns={columns} dataSource={data} />;
+export default Team;
