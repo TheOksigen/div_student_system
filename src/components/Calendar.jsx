16551881ff20ed0aaaa2327324bf22aba6f99634
TheOksigen <david@div.edu.az>
2023-09-30 18:41:21 +0400

    modified:   src/components/Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -18,6 +18,7 @@ export default function Calendar() {
       },
       {
         startTime: new Date("2023-09-29,12:00:00"),
+        
         endTime: new Date("2023-09-29,15:00:00"),
         title: "asdadfasvfs",
         backgroundColor: "red",
